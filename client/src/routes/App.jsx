2c6fb5f1f96effa53a68e09ffdbedfc4f9340f9c
GiovanniCaiazzo01 <giovannicaiazzo002@gmail.com>
2023-05-07 16:29:03 +0200

    wip: starting HomePage

--- client/src/routes/App.jsx
+++ client/src/routes/App.jsx
@@ -1,7 +1,11 @@
-import loadable from "@loadable/component";
 import { Routes, Route } from "react-router-dom";
 import { PublicRoute } from "../components/";
+import loadable from "@loadable/component";
 import "./App.css";
+
+const HomePage = loadable(() => import("../pages/"), {
+  resolveComponent: (components) => components.HomePage,
+});
 
 const MainRoute = () => {
   return (
