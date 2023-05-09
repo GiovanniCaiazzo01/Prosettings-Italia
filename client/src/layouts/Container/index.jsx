635e5fda9458df51f99a9ac039ea19d208b34e92
GiovanniCaiazzo01 <giovannicaiazzo002@gmail.com>
2023-05-09 15:56:52 +0200

    refactor: added width and height props to <Container/>

--- client/src/layouts/Container/index.jsx
+++ client/src/layouts/Container/index.jsx
@@ -1,6 +1,10 @@
 import { StyledContainer } from "./style";
 import PropTypes from "prop-types";
 const Container = ({
+  width,
+  maxWidth,
+  height,
+  maxHeight,
   padding,
   paddingTop,
   paddingBottom,
@@ -17,6 +21,10 @@ const Container = ({
 }) => {
   return (
     <StyledContainer
+      width={width}
+      maxWidth={maxWidth}
+      height={height}
+      maxHeight={maxHeight}
       padding={padding}
       paddingTop={paddingTop}
       paddingBottom={paddingBottom}
@@ -38,6 +46,10 @@ const Container = ({
 export { Container };
 
 Container.propTypes = {
+  width: PropTypes.number,
+  maxWidth: PropTypes.number,
+  height: PropTypes.number,
+  maxHeight: PropTypes.number,
   padding: PropTypes.number,
   paddingTop: PropTypes.number,
   paddingBottom: PropTypes.number,
