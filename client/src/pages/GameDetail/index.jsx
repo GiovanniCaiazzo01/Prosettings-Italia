6c4ec494ba4a7e5e780b97d82875314c30c79ad5
GiovanniCaiazzo01 <giovannicaiazzo002@gmail.com>
2023-05-31 10:46:55 +0200

    wip: game detail

--- client/src/pages/GameDetail/index.jsx
+++ client/src/pages/GameDetail/index.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 // import PropTypes from "prop-types";
-import { Container, Flex, Img } from "../../layouts";
+import { Container, Flex, Heading, Img } from "../../layouts";
 import { useParams } from "react-router-dom";
 
 const GameDetail = () => {
   const { game } = useParams();
   return (
     <Container padding={20}>
-      <Flex alignItems="center">
-        <Img src={game} />
+      <Flex>
+        <Img src={game} width={150} />
+        <Flex marginLeft={10} column justifyContent="space-around">
+          <Heading color="000000">{game.toUpperCase()}</Heading>
+          <Flex alignItems="center" justifyContent="space-between">
+            <Heading color="000000" fontSize={1}>
+              222 players
+            </Heading>
+            <Heading color="000000" fontSize={1}>
+              222 players
+            </Heading>
+          </Flex>
+        </Flex>
       </Flex>
     </Container>
   );
