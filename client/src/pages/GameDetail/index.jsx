d1f8f9a0a2c04816be80ce89c3d71e1eed2408aa
GiovanniCaiazzo01 <giovannicaiazzo002@gmail.com>
2023-05-31 11:33:54 +0200

    wip: game detail base structure

--- client/src/pages/GameDetail/index.jsx
+++ client/src/pages/GameDetail/index.jsx
@@ -6,19 +6,36 @@ import { useParams } from "react-router-dom";
 const GameDetail = () => {
   const { game } = useParams();
   return (
-    <Container padding={20}>
-      <Flex>
+    <Container
+      padding={20}
+      backgroundColor="fff"
+      marginTop={20}
+      borderRadius={15}
+      maxWidth={600}
+    >
+      <Flex justifyContent="center">
         <Img src={game} width={150} />
-        <Flex marginLeft={10} column justifyContent="space-around">
+        <Flex
+          marginLeft={30}
+          column
+          justifyContent="space-around"
+          alignItems="space-around"
+        >
           <Heading color="000000">{game.toUpperCase()}</Heading>
-          <Flex alignItems="center" justifyContent="space-between">
-            <Heading color="000000" fontSize={1}>
-              222 players
-            </Heading>
-            <Heading color="000000" fontSize={1}>
-              222 players
-            </Heading>
+          <Flex alignItems="center">
+            <Flex alignItems="flex-start" justifyContent="space-around">
+              <div style={{ color: "#000000" }}>ciao</div>
+              <div style={{ color: "#000000" }}>ciao</div>
+            </Flex>
+            <Flex alignItems="center" justifyContent="space-around">
+              <div style={{ color: "#000000" }}>ciao</div>
+              <div style={{ color: "#000000" }}>ciao</div>
+            </Flex>
           </Flex>
+          <Heading fontSize={0.6} color="000000">
+            VALORANT is a free-to-play first-person hero shooter developed and
+            published by Riot Games. It’s 5v5, competitive, and character-based.
+          </Heading>
         </Flex>
       </Flex>
     </Container>
